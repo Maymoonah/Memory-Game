5714c3565fe3d5d9e2578dc7053bf3e3ed6a13b6
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2017-12-03 12:25:42 -0800

    remove all extra white spaces from app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,9 @@
 //Create array to hold all the cards
- let cardsArray = [];
- $('.deck').find('li').each(function() {
- 	cardsArray.push(this);
+let cardsArray = [];
+$('.deck').find('li').each(function() {
+	cardsArray.push(this);
 });
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+
 //shuffle the list of cards using the "shuffle" method
 shuffle(cardsArray);
 //append cards to deck using append method
@@ -23,29 +18,18 @@ function appendCards() {
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
+	let currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
 
-    return array;
+	return array;
 }
-
-/*
-* set up the event listener for a card. If a card is clicked:
-*  - display the card's symbol (put this functionality in another function that you call from this one)
-*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
-*  - if the list already has another card, check to see if the two cards match
-*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
-*    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
-*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
-*    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
-*/
 
 //create variables and openCardsList array to hold open cards
 let openCardsList = [];
@@ -59,12 +43,12 @@ $('.card').on('click', function() {
 	displayCard(this);
 	addOpenCard(this);
 	//add card to openCardsList
-	openCardsList.push(this);	
+	openCardsList.push(this);
 	//start timer only after first click
 	if(timesClicked === 1){
 		gameTimer();
 	}
-	starRating();	
+	starRating();
 });
 
 //function to display card symbol
@@ -73,7 +57,7 @@ function displayCard(card) {
 }
 
 //function to add open cards to a list
-function addOpenCard(card) {;
+function addOpenCard(card) {
 	//check if there are two cards in the list
 	if(openCardsList.length === 2) {
 		if(openCardsList[0].innerHTML === openCardsList[1].innerHTML) {
@@ -139,11 +123,11 @@ function gameWon() {
 	}
 	if($('li.match').length === 16) {
 		alert(
-			`Congratulations ${player}! Your Score: ${score}!`  
+			`Congratulations ${player}! Your Score: ${score}!`
 			);
 	}
 	//stop timer
-	stopTimer();	
+	stopTimer();
 }
 
 //Star rating
@@ -155,22 +139,22 @@ function starRating() {
 	if(numMoves > 8 && numMoves <= 15) {
 		stars.last().css('color', '#000');
 	}
-	//if number of moves is greater than 15, change rating to 1 star 
+	//if number of moves is greater than 15, change rating to 1 star
 	else if(numMoves > 15) {
 		stars.css('color', '#000');
 		stars.first().css('color', '#ffe500');
 	}
 }
 
-//gameTimer function 
+//gameTimer function
 function gameTimer() {
 	//Stack overflow: https://stackoverflow.com/questions/5517597/plain-count-up-timer-in-javascript
 	sec = 0;
-    function pad ( val ) { return val > 9 ? val : "0" + val; }
-    timer = setInterval( function(){    
-        document.getElementById("timer").innerHTML= "Time Played: " +
-        pad(parseInt(sec/60,10)) + ":" + pad(++sec%60);
-    }, 1000); 
+	function pad ( val ) { return val > 9 ? val : "0" + val; }
+	timer = setInterval( function(){
+	document.getElementById("timer").innerHTML= "Time Played: " +
+	pad(parseInt(sec/60,10)) + ":" + pad(++sec%60);
+	}, 1000);
 }
 
 //stop the game timer
